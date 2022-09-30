2da2b8133812b9bd67c5ad1a2af14283d8228ed3
naiiiden <na4ko359@gmail.com>
2022-09-30 11:38:39 +0300

    toggle add label <span> for sr

--- script/script.js
+++ script/script.js
@@ -1,3 +1,8 @@
 document.getElementById("toggle").addEventListener("click", () => {
     document.getElementById("show").classList.toggle("show_monthly");
+    if (document.querySelector(".sr-only").textContent == "Monthly pricing") {
+        document.querySelector(".sr-only").textContent = "Anually pricing";
+    } else {
+        document.querySelector(".sr-only").textContent = "Monthly pricing";
+    };
 });
